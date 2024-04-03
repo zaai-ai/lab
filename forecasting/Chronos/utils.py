d73132fcfc022dd5bb76b82bba1e54dd1745853f
rafael guedes <rafael.guedes@marleyspoon.com>
2024-04-03 15:55:55 +0100

    tourism

--- forecasting/Chronos/utils.py
+++ forecasting/Chronos/utils.py
@@ -9,19 +9,61 @@ import torch
 from chronos import ChronosPipeline
 
 
+def preprocess_dataset(dataframe: pd.DataFrame, dynamic_cov: list, time_col: str, target: str) -> pd.DataFrame:
+    """
+    Receives the raw dataframe and creates:
+        - unique id column
+        - make dynamic start dates for each series based on the first date where visits is different than 0
+        - create 4 static covariates based on the type of tourism
+    Args:
+        dataframe (pd.DataFrame): raw data
+        dynamic_cov (list): column names with dynamic cov
+        time_col (str): time column name
+        target (str): target name
+    Returns:
+        pd.DataFrame: cleaned data
+    """
+    
+    # save dynamic cov for later
+    dynamic_cov_df = dataframe[dynamic_cov].reset_index().drop_duplicates()
+    
+    # create target and unique id columns
+    dataframe = dataframe.loc[:, ~dataframe.columns.isin(dynamic_cov)].melt(ignore_index=False).reset_index().rename(columns={'variable':'unique_id', 'value':target})
+    
+    # crete dynamic start dates for each series
+    cleaned_df = []
+    for i in dataframe['unique_id'].unique():
+        temp = dataframe[dataframe['unique_id'] == i]
+        cleaned_df.append(temp[temp[time_col] >= min(temp[temp[target] > 0][time_col])])
+    cleaned_df = pd.concat(cleaned_df)
+    
+    # join dynamic cov
+    cleaned_df = pd.merge(cleaned_df, dynamic_cov_df, on=[time_col], how='left')
+    
+    # extract static covariates
+    cleaned_df['static_1'] = cleaned_df['unique_id'].apply(lambda x: x[0])
+    cleaned_df['static_2'] = cleaned_df['unique_id'].apply(lambda x: x[1])
+    cleaned_df['static_3'] = cleaned_df['unique_id'].apply(lambda x: x[2])
+    cleaned_df['static_4'] = cleaned_df['unique_id'].apply(lambda x: x[3:])
+    
+    return cleaned_df
+
+
 def plot_model_comparison(dataframe: pd.DataFrame) -> None:
     """
     Bar plot comparison between models
     Args:
         dataframe (pd.DataFrame): data with actuals and forecats for both models
     """
+    
+    dataframe['Date'] = dataframe['Date'].dt.date
 
     tide_model = dataframe.rename(
         columns={"TiDE": "forecast"}
     )
     tide_model["model"] = "TiDE"
     tide_model["MAPE"] = (
-        abs(tide_model["sales"] - tide_model["forecast"]) / tide_model["sales"]
+        abs(tide_model["visits"] - tide_model["forecast"]) / tide_model["visits"]
     )
 
     chronos_tiny_model = dataframe.rename(
@@ -29,8 +71,8 @@ def plot_model_comparison(dataframe: pd.DataFrame) -> None:
     )
     chronos_tiny_model["model"] = "Chronos Tiny"
     chronos_tiny_model["MAPE"] = (
-        abs(chronos_tiny_model["sales"] - chronos_tiny_model["forecast"])
-        / chronos_tiny_model["sales"]
+        abs(chronos_tiny_model["visits"] - chronos_tiny_model["forecast"])
+        / chronos_tiny_model["visits"]
     )
 
     chronos_large_model = dataframe.rename(
@@ -38,8 +80,8 @@ def plot_model_comparison(dataframe: pd.DataFrame) -> None:
     )
     chronos_large_model["model"] = "Chronos Large"
     chronos_large_model["MAPE"] = (
-        abs(chronos_large_model["sales"] - chronos_large_model["forecast"])
-        / chronos_large_model["sales"]
+        abs(chronos_large_model["visits"] - chronos_large_model["forecast"])
+        / chronos_large_model["visits"]
     )
 
     plt.rcParams["figure.figsize"] = (20, 5)
@@ -47,19 +89,19 @@ def plot_model_comparison(dataframe: pd.DataFrame) -> None:
         data=pd.concat(
             [tide_model, chronos_tiny_model, chronos_large_model]
         ),
-        x="year_month",
+        x="Date",
         y="MAPE",
         hue="model",
-        palette=["#dd4fe4", "#070620", "#8a70be"],
+        palette=["#dd4fe4", "#070620", "#8a70be", "#fa7302"],
     )
-    plt.title("Comparison between TiDE and Chronos in EV data")
+    plt.title("Comparison between TiDE and Chronos in Tourism data")
     plt.xticks(rotation=45)
     ax.yaxis.set_major_formatter(mtick.PercentFormatter(1.0))
     plt.show()
 
 
 def chronos_forecast(
-    model: ChronosPipeline, data: pd.DataFrame, horizon: int
+    model: ChronosPipeline, data: pd.DataFrame, horizon: int, target: str
 ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     """
     Generates forecast with Chronos
@@ -67,12 +109,13 @@ def chronos_forecast(
         model (ChronosPipeline): pre-trained model
         data (pd.DataFrame): historical data
         horizon (int): forecast horizon
+        target (str): target column
     Returns:
         Tuple[np.ndarray, np.ndarray, np.ndarray]: lower, mid and upper forecast values
     """
     # context must be either a 1D tensor, a list of 1D tensors,
     # or a left-padded 2D tensor with batch as the first dimension
-    context = torch.tensor(data["sales"].tolist())
+    context = torch.tensor(data[target].tolist())
     forecast = model.predict(
         context, horizon
     )  # shape [num_series, num_samples, prediction_length]
@@ -92,7 +135,7 @@ def convert_forecast_to_pandas(
         pd.DataFrame: forecast in pandas format
     """
 
-    forecast_pd = holdout_set[["Car_category", "year_month"]]
+    forecast_pd = holdout_set[["unique_id", "Date"]]
     forecast_pd.loc[:, "forecast_lower"] = forecast[0]
     forecast_pd.loc[:, "forecast"] = forecast[1]
     forecast_pd.loc[:, "forecast_upper"] = forecast[2]
@@ -113,19 +156,19 @@ def plot_actuals_forecast(
 
     plt.figure(figsize=(20, 5))
     plt.plot(
-        actuals_data["year_month"],
-        actuals_data["sales"],
+        actuals_data["Date"],
+        actuals_data["visits"],
         color="royalblue",
         label="historical data",
     )
     plt.plot(
-        forecast_data["year_month"],
+        forecast_data["Date"],
         forecast_data["forecast"],
         color="tomato",
         label="median forecast",
     )
     plt.fill_between(
-        forecast_data["year_month"],
+        forecast_data["Date"],
         forecast_data["forecast_lower"],
         forecast_data["forecast_upper"],
         color="tomato",
@@ -161,7 +204,7 @@ def transform_predictions_to_pandas(predictions: list, target: str, pred_list: l
         temp["forecast_upper"] = p.quantile_df(quantiles[2]).reset_index()[f"{target}_{quantiles[2]}"]
 
         # add unique id
-        temp["Car_category"] = list(pdf.static_covariates_values())[0][0]
+        temp["unique_id"] = list(pdf.static_covariates_values())[0][0]+list(pdf.static_covariates_values())[0][1]+list(pdf.static_covariates_values())[0][2]+list(pdf.static_covariates_values())[0][3]
 
         # convert negative predictions into 0
         temp[["forecast", "forecast_lower", "forecast_upper"]] = temp[
